47743d6f016b0769acb1f94668daeef238792333
Marius Wörfel <raborogit@gmail.com>
2024-10-12 00:36:12 +0200

    init(L2Project): :tada: Init app for L2

---
+++ L2CPControls/app/src/main/java/de/dhbw/mobapp/l2cpcontrols/MainActivity.kt
@@ -0,0 +1,48 @@
+package de.dhbw.mobapp.l2cpcontrols
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import de.dhbw.mobapp.l2cpcontrols.ui.theme.L2CPControlsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            L2CPControlsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier.padding(20.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    L2CPControlsTheme {
+        Greeting("Android")
+    }
+}
