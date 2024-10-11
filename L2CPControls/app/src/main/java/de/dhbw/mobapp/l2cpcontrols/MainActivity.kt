08efd0792b065a7c9181cfa6765862e50e493dad
Marius Wörfel <raborogit@gmail.com>
2024-10-12 00:45:14 +0200

    refactor(L2): :recycle: Add text with StringR into column with styling & Remove preview

--- L2CPControls/app/src/main/java/de/dhbw/mobapp/l2cpcontrols/MainActivity.kt
+++ L2CPControls/app/src/main/java/de/dhbw/mobapp/l2cpcontrols/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import de.dhbw.mobapp.l2cpcontrols.ui.theme.L2CPControlsTheme
 
@@ -33,16 +37,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier.padding(20.dp)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    L2CPControlsTheme {
-        Greeting("Android")
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(24.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.hello),
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        Text(
+            text = "Hello $name!",
+            color = MaterialTheme.colorScheme.onPrimary,
+        )
     }
 }
