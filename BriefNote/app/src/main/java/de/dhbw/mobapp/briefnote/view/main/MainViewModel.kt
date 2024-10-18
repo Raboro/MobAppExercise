1b252ef59e3d2cb7d63bd08f5661fe8348124874
Marius Wörfel <raborogit@gmail.com>
2024-10-18 08:50:27 +0200

    feat(L3): :sparkles: Add MainViewModel

---
+++ BriefNote/app/src/main/java/de/dhbw/mobapp/briefnote/view/main/MainViewModel.kt
@@ -0,0 +1,25 @@
+package de.dhbw.mobapp.briefnote.view.main
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import de.dhbw.mobapp.briefnote.database.Note
+import de.dhbw.mobapp.briefnote.database.NoteDatabase
+import de.dhbw.mobapp.briefnote.database.NoteRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private lateinit var repository: NoteRepository // in reality dependency injection
+
+    var noteText by mutableStateOf("")
+
+    fun initialize(database: NoteDatabase) {
+        repository = NoteRepository(database.noteDao)
+    }
+
+    fun insert() = viewModelScope.launch {
+        repository.insert(Note(0, noteText)) // id 0 -> auto generated
+    }
+}
