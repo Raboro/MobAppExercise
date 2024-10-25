41c1befc39a8d676cf3dbf2a3abb349e44d46233
Marius Wörfel <raborogit@gmail.com>
2024-10-25 10:34:59 +0200

    refactor(L4): :art: Add styling

--- GoRest/app/src/main/java/de/dhbw/mobapp/gorest/view/main/cell/UserCell.kt
+++ GoRest/app/src/main/java/de/dhbw/mobapp/gorest/view/main/cell/UserCell.kt
@@ -1,14 +1,29 @@
 package de.dhbw.mobapp.gorest.view.main.cell
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import de.dhbw.mobapp.gorest.dto.UserDto
 
 @Composable
 fun UserCell(userDto: UserDto) {
-    Column {
-        Text("${userDto.id}")
-        Text(userDto.name)
+    Card(
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Column(modifier = Modifier.padding(5.dp)) {
+            Text("${userDto.id}")
+            Text(userDto.name)
+        }
     }
 }
