09ef49302a9a3f3c2f9d03ec6198ee216e22a701
Marius Wörfel <raborogit@gmail.com>
2024-10-25 09:13:10 +0200

    feat(l4): :sparkles: Add get/put/delete user

--- GoRest/app/src/main/java/de/dhbw/mobapp/gorest/service/UserService.kt
+++ GoRest/app/src/main/java/de/dhbw/mobapp/gorest/service/UserService.kt
@@ -3,9 +3,12 @@ package de.dhbw.mobapp.gorest.service
 import de.dhbw.mobapp.gorest.api.ApiManager
 import de.dhbw.mobapp.gorest.dto.UserDto
 import io.ktor.client.call.body
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
 
 class UserService(private val apiManager: ApiManager = ApiManager()) {
 
@@ -17,5 +20,19 @@ class UserService(private val apiManager: ApiManager = ApiManager()) {
         return apiManager.jsonHttpClient.post("users") {
             setBody(userDto)
         }.body()
+    }
+
+    suspend fun getUser(usedId: Int): UserDto {
+        return apiManager.jsonHttpClient.get("users/$usedId").body();
+    }
+
+    suspend fun updateUser(userDto: UserDto): UserDto {
+        return apiManager.jsonHttpClient.put("users/${userDto.id}") {
+            setBody(userDto)
+        }.body()
+    }
+
+    suspend fun deleteUser(usedId: Int): HttpResponse {
+        return apiManager.jsonHttpClient.delete("users/$usedId")
     }
 }
