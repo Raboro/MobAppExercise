bc18e7332e8c6a499c926104d43fd7d4b0dd77f3
Marius Wörfel <raborogit@gmail.com>
2024-10-25 10:37:39 +0200

    refactor(ÃL4): :art: Add styling

--- GoRest/app/src/main/java/de/dhbw/mobapp/gorest/view/main/MainView.kt
+++ GoRest/app/src/main/java/de/dhbw/mobapp/gorest/view/main/MainView.kt
@@ -1,6 +1,7 @@
 package de.dhbw.mobapp.gorest.view.main
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -9,6 +10,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -28,10 +30,15 @@ fun MainView(navController: NavController, mainViewModel: MainViewModel) {
         Button(onClick = { navController.navigate("userDetailView") }) {
             Text(text = stringResource(R.string.new_user))
         }
-        if (mainViewModel.loading) {
-            CircularProgressIndicator()
-        } else {
-            MainList(navController = navController, mainViewModel = mainViewModel)
+
+        Column(modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth()) {
+            if (mainViewModel.loading) {
+                CircularProgressIndicator(modifier = Modifier.align(CenterHorizontally))
+            } else {
+                MainList(navController = navController, mainViewModel = mainViewModel)
+            }
         }
     }
 }
