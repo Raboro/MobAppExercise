d6aa7938a71ca38369ce25758476376e99cf1c73
Marius Wörfel <raborogit@gmail.com>
2024-10-25 09:36:14 +0200

    feat(l4): :sparkles: Add styling & string resource

--- GoRest/app/src/main/java/de/dhbw/mobapp/gorest/view/main/MainView.kt
+++ GoRest/app/src/main/java/de/dhbw/mobapp/gorest/view/main/MainView.kt
@@ -1,13 +1,21 @@
 package de.dhbw.mobapp.gorest.view.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import de.dhbw.mobapp.gorest.R
 
 @Composable
 fun MainView(navController: NavController) {
-    Button(onClick = { navController.navigate("userDetailView") }) {
-        Text(text = "Neuen Nutzer hinzufügen")
+    Column(modifier = Modifier.padding(5.dp)) {
+        Button(onClick = { navController.navigate("userDetailView") }) {
+            Text(text = stringResource(R.string.new_user))
+        }
     }
 }
--- GoRest/app/src/main/java/de/dhbw/mobapp/gorest/view/userDetail/UserDetailView.kt
+++ GoRest/app/src/main/java/de/dhbw/mobapp/gorest/view/userDetail/UserDetailView.kt
@@ -1,9 +1,12 @@
 package de.dhbw.mobapp.gorest.view.userDetail
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun UserDetailView() {
-    Text(text = "Hello DHBW")
+    Text(text = "Hello DHBW", modifier = Modifier.padding(5.dp))
 }
